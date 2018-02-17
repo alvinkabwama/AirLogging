48b439f6af44b371a1e1f5b19421fcd48bdf8651
alvinkabwama <alkaleos10@gmail.com>
2018-02-17 14:35:04 +0300

    Reverse

--- dataview/views.py
+++ dataview/views.py
@@ -7,7 +7,7 @@ def datashow(request):
     if(request.method == 'GET'):
         serialnumber = '0645'
         selectedevice =  Device.objects.get(deviceserial = serialnumber)
-        devicedata = Data.objects.filter(device = selectedevice)
+        devicedata = Data.objects.filter(device = selectedevice).order_by("-pk")
         context = {'devicedata': devicedata}
         return render(request, 'tableview.html', context)
         
